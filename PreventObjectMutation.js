18f37c517eea94ab6dd539c231cc15ae613df258
Ronan500 <Ronandaly500@gmail.com>
2018-08-13 23:25:27 +0100

    Add more files

---
+++ PreventObjectMutation.js
@@ -0,0 +1,36 @@
+// Prevent object mutation.....
+
+
+
+function freezeObj() {
+  "use strict";
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+
+// Capital "O" in the word "Object" here...
+  Object.freeze(MATH_CONSTANTS);
+
+
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch( ex ) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+
+
+
+
+/*
+  As seen in the previous challenge, const declaration alone
+  doesn't really protect your data from mutation. To ensure
+  your data doesn't change, JavaScript provides a function Object.
+  freeze to prevent data mutation.
+  Once the object is frozen, you can no longer add, update,
+  or delete properties from it. Any attempt at changing
+  the object will be rejected without an error.
+
+*/
